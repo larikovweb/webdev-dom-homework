f56ce84e6edb5ab5fb0eea8f78cf30f3ab63538d
Ura <ura-lar@mail.ru>
2023-05-12 12:41:00 +0300

    feat: 2.12

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const commentsArray = localStorage.getItem('comments')
-  ? JSON.parse(localStorage.getItem('comments'))
-  : [];
+const _apiUrl = 'https://webdev-hw-api.vercel.app/api/v1/:yurii-larikov/comments';
 
-const saveLocaleStorage = (key, array) => {
-  localStorage.setItem(key, JSON.stringify(array));
+const useHTTP = (method, url, func, body) => {
+  fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .then(func);
 };
+
+let commentsArray = [];
+
 const protectionInnerHTML = (text) => {
   return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 };
@@ -14,7 +20,7 @@ class CommentForm {
       throw new Error('Element is not an HTMLElement');
     }
     this.commentList = commentList;
-    this.value = { name: '', comment: '', date: '', like: { count: 0, active: false } };
+    this.value = { author: { name: '' }, name: '', text: '', date: '', likes: 0, isLiked: false };
     this.element = element;
     this.isValid = false;
     this.input = this.element.querySelector('input');
@@ -30,23 +36,30 @@ class CommentForm {
   onSubmit() {
     if (!this.isValid) return;
     this.value.date = new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString();
-    commentsArray.push(this.value);
-    saveLocaleStorage('comments', commentsArray);
-    const newComment = new Comment(this.value).render();
-    this.commentList.appendChild(newComment);
-    this.reset();
-    this.input.focus();
+    useHTTP(
+      'POST',
+      _apiUrl,
+      (data) => {
+        commentsArray = data.comments;
+        const newComment = new Comment(this.value).render();
+        this.commentList.appendChild(newComment);
+        this.reset();
+        this.input.focus();
+      },
+      this.value,
+    );
   }
   update() {
-    this.input.value = this.value.name;
-    this.textarea.value = this.value.comment;
-    this.isValid = this.value.name && this.value.comment;
+    this.input.value = this.value.author.name;
+    this.textarea.value = this.value.text;
+    this.isValid = this.value.author.name && this.value.text;
     this.disableBtn();
   }
   onChange() {
+    this.value.author.name = this.input.value;
     this.value.name = this.input.value;
-    this.value.comment = this.textarea.value;
-    this.isValid = this.value.name && this.value.comment;
+    this.value.text = this.textarea.value;
+    this.isValid = this.value.author.name && this.value.text;
     this.disableBtn();
   }
   onPressEnter(event) {
@@ -60,7 +73,7 @@ class CommentForm {
       : this.button.setAttribute('disabled', true);
   }
   reset() {
-    this.value = { name: '', comment: '', date: '', like: { count: 0, active: false } };
+    this.value = { author: { name: '' }, name: '', text: '', date: '', likes: 0, isLiked: false };
     this.input.value = '';
     this.textarea.value = '';
     this.isValid = false;
@@ -82,22 +95,23 @@ class Comment {
   }
 }
 Comment.commentTemplate = (value) => {
-  const { name, comment, date, like } = value;
+  console.log(value);
+  let { author, text, date, likes, isLiked } = value;
   const wrapper = document.createElement('div');
   wrapper.classList.add('comment');
   wrapper.innerHTML = `
   <i class="comment-remove"></i>
   <div class="comment-header">
-    <div>${protectionInnerHTML(name)}</div>
+    <div>${protectionInnerHTML(author.name)}</div>
     <div>${date}</div>
   </div>
   <div class="comment-body">
-    <pre class="comment-text">${protectionInnerHTML(comment)}</pre>
+    <pre class="comment-text">${protectionInnerHTML(text)}</pre>
   </div>
   <div class="comment-footer">
     <div class="likes">
-      <span class="likes-counter">${like.count}</span>
-      <button class="like-button ${like.active && '-active-like'}"></button>
+      <span class="likes-counter">${likes}</span>
+      <button class="like-button ${isLiked && '-active-like'}"></button>
     </div>
   </div>
   `;
@@ -107,7 +121,7 @@ Comment.commentTemplate = (value) => {
   const removeButton = wrapper.querySelector('.comment-remove');
 
   const onAnswer = () => {
-    commentClass.value.comment = `> ${comment} \r\n ${name},`;
+    commentClass.value.text = `> ${comment} \r\n ${name},`;
     commentClass.update();
     commentClass.input.focus();
   };
@@ -116,22 +130,20 @@ Comment.commentTemplate = (value) => {
     e.stopPropagation();
     likeButton.classList.toggle('-active-like');
     if (likeButton.classList.contains('-active-like')) {
-      like.active = true;
-      like.count++;
+      isLiked = true;
+      likes++;
       likesCounter.textContent++;
     } else {
-      like.active = false;
-      like.count--;
+      isLiked = false;
+      likes--;
       likesCounter.textContent--;
     }
-    saveLocaleStorage('comments', commentsArray);
   };
 
   const onRemove = (e) => {
     e.stopPropagation();
     wrapper.remove();
     commentsArray.splice(commentsArray.indexOf(value), 1);
-    saveLocaleStorage('comments', commentsArray);
   };
 
   wrapper.addEventListener('click', onAnswer);
@@ -145,4 +157,8 @@ const commentList = document.querySelector('.comments');
 const commentForm = document.querySelector('.add-form');
 
 const commentClass = new CommentForm(commentForm, commentList);
-commentClass.renderComments();
+useHTTP('GET', _apiUrl, (data) => {
+  commentsArray = data.comments;
+  console.log(commentsArray);
+  commentClass.renderComments();
+});
