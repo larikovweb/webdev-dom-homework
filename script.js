44ac559bc9cf0302debcb7aea6539df971b1d4b1
Ura <ura-lar@mail.ru>
2023-05-21 11:27:12 +0300

    fix: 2.14

--- script.js
+++ script.js
@@ -6,18 +6,21 @@ const useHTTP = (method, url, func, body) => {
     body: JSON.stringify(body),
   })
     .then((res) => {
-      if (res.status === 500) {
-        throw new Error('Ошибка сервера, попробуйте позже');
-      } else if (res.status === 400) {
-        throw new Error('Имя и комментарий должны быть не менее 3 символов');
-      } else {
+      console.log(res);
+      if (res.ok) {
         return res.json();
+      } else if (res.status >= 500 && res.status < 600) {
+        throw new Error('Сервер сломался, попробуй позже');
+      } else if (res.status === 400) {
+        throw new Error('Имя и комментарий должны быть не короче 3 символов');
       }
     })
     .then(func)
     .catch((err) => {
       console.warn(err);
-      alert('Кажется что-то пошло не так, попробуйте позже');
+      alert(
+        err.message === 'Failed to fetch' ? 'Что-то пошло не так, попробуйте позже' : err.message,
+      );
       func();
     });
 };
